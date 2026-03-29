2d39f29ea383d4640ede5a26258916441cdd498a
Abdulrahman <syamabdo382@gmail.com>
2026-03-29 11:37:52 +0200

    init Firebase in main and call setupServiceLocator method

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:stock_mate/core/di/service_locator.dart';
 import 'package:stock_mate/core/theme/theme_data/app_theme_dark_mode.dart';
 import 'package:stock_mate/features/splash/presentation/views/splash_view.dart';
+import 'package:stock_mate/firebase_options.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  setupServiceLocator();
   runApp(const StockMate());
 }
 
