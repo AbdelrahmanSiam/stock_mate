9390f1877bb28c31e50eee5945e3de5204058c62
Abdulrahman <syamabdo382@gmail.com>
2026-03-29 11:39:01 +0200

    provide SplashCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stock_mate/core/di/service_locator.dart';
 import 'package:stock_mate/core/theme/theme_data/app_theme_dark_mode.dart';
+import 'package:stock_mate/features/splash/presentation/manager/cubits/splash_cubit/splash_cubit.dart';
 import 'package:stock_mate/features/splash/presentation/views/splash_view.dart';
 import 'package:stock_mate/firebase_options.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupServiceLocator();
   runApp(const StockMate());
 }
@@ -19,10 +19,13 @@ class StockMate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppThemeDarkMode.getDarkTheme(),
-      home: const SplashView(),
+    return BlocProvider(
+      create: (context) => getIt<SplashCubit>(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppThemeDarkMode.getDarkTheme(),
+        home: const SplashView(),
+      ),
     );
   }
 }
