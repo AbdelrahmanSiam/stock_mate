c033348c0585ffd34b5834a1305dd61f14dffeca
Abdulrahman <syamabdo382@gmail.com>
2026-03-30 11:53:57 +0200

    create SaveLanguageUseCase class

---
+++ lib/core/usecases/save_language_use_case.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:stock_mate/core/errors/failure.dart';
+import 'package:stock_mate/core/usecases/usecase.dart';
+import 'package:stock_mate/features/language/domain/repo/language_repo.dart';
+
+class SaveLanguageParameters {
+  final String languageCode;
+
+  SaveLanguageParameters({required this.languageCode});
+}
+
+class SaveLanguageUseCase implements UseCase<void, SaveLanguageParameters> {
+  final LanguageRepo languageRepo;
+
+  SaveLanguageUseCase({required this.languageRepo});
+  @override
+  Future<Either<Failure, void>> call([
+    SaveLanguageParameters? parameter,
+  ]) async {
+    try {
+      await languageRepo.saveLanguage(
+        languageCode: parameter!.languageCode,
+      ); // parameter is obj from SaveLanguageParameters class
+      return const Right(null); // save it and return nothing
+    } catch (e) {
+      return Left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}
