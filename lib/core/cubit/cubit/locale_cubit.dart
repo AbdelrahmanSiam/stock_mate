65c475d48f64f3b91dbee877092f9b393e167c3f
Abdulrahman <syamabdo382@gmail.com>
2026-03-31 08:11:12 +0200

    create changeLocal to call it when user use language screen from app to change it

--- lib/core/cubit/cubit/locale_cubit.dart
+++ lib/core/cubit/cubit/locale_cubit.dart
@@ -12,7 +12,7 @@ class LocaleCubit extends Cubit<LocaleState> {
 
   final SaveLanguageUseCase saveLanguageUseCase;
   final GetSavedLanguageUseCase getSavedLanguageUseCase;
-          // call it main to return saved language when open app every time
+  // call it main to return saved language when open app every time
   Future<void> loadSavedLanguage() async {
     final result = await getSavedLanguageUseCase();
     result.fold((_) => emit(LocaleLoadedState(locale: Locale("ar"))), (
@@ -25,5 +25,13 @@ class LocaleCubit extends Cubit<LocaleState> {
         emit(LocaleLoadedState(locale: Locale(languageCode)));
       }
     });
+  }
+        // Called when user use language screen from app to change it
+  Future<void> changeLocal(String languageCode) async {
+    final result = await saveLanguageUseCase();
+    result.fold(
+      (_) => null,
+      (_) => emit(LocaleLoadedState(locale: Locale(languageCode))),
+    );
   }
 }
