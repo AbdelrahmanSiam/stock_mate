459142a3893065c2b46a167c0758be44598e214b
Abdulrahman <syamabdo382@gmail.com>
2026-03-31 17:15:51 +0200

    finish CustomAuthTextField

--- lib/features/auth/presentation/views/widgets/custom_auth_text_field.dart
+++ lib/features/auth/presentation/views/widgets/custom_auth_text_field.dart
@@ -1,32 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:stock_mate/core/styles/app_styles.dart';
 import 'package:stock_mate/core/theme/app_colors/app_colors_dark_mode.dart';
-import 'package:stock_mate/core/theme/theme_data/app_theme_dark_mode.dart';
 
 class CustomAuthTextField extends StatelessWidget {
+  final String label, hint;
+  final IconData prefixIcon;
+  final Widget widget;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+  final void Function(String)? onChanged;
+  final TextEditingController? controller;
+
   const CustomAuthTextField({
     super.key,
     required this.label,
     required this.hint,
+    required this.prefixIcon,
+    this.widget = const SizedBox(),
+    this.obscureText = false,
+    this.suffixIcon,
+    this.keyboardType = TextInputType.text,
+    this.onChanged,
+    this.controller,
   });
-  final String label, hint;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          label,
-          style: AppStyles.labelSemiBold13(
-            context,
-          ).copyWith(color: AppColorsDarkMode.textSecondary),
+        Row(
+          children: [
+            Text(
+              label,
+              style: AppStyles.labelSemiBold13(
+                context,
+              ).copyWith(color: AppColorsDarkMode.textSecondary),
+            ),
+            Spacer(),
+            widget,
+          ],
         ),
         SizedBox(height: 8),
         TextField(
+          controller: controller,
+          obscureText: obscureText,
+          keyboardType: keyboardType,
+          onChanged: onChanged,
           decoration: InputDecoration(
-            prefixIcon: Icon(Icons.email_outlined),
+            prefixIcon: Icon(prefixIcon),
             hintText: hint,
-            suffix: Icon(Icons.remove_red_eye),
+            suffixIcon: suffixIcon,
           ),
         ),
       ],
--- lib/features/auth/presentation/views/widgets/login_card_section.dart
+++ lib/features/auth/presentation/views/widgets/login_card_section.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:stock_mate/core/styles/app_styles.dart';
+import 'package:stock_mate/core/theme/app_colors/app_colors_dark_mode.dart';
+import 'package:stock_mate/core/theme/theme_data/app_theme_dark_mode.dart';
 import 'package:stock_mate/core/utils/widgets/app_card.dart';
 import 'package:stock_mate/features/auth/presentation/views/widgets/custom_auth_text_field.dart';
 import 'package:stock_mate/generated/l10n.dart';
 
 class LoginCardSection extends StatelessWidget {
-  const LoginCardSection({
-    super.key,
-  });
+  const LoginCardSection({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +19,17 @@ class LoginCardSection extends StatelessWidget {
           CustomAuthTextField(
             label: S.of(context).email,
             hint: "mohamed@gmail.com",
+            prefixIcon: Icons.email_outlined,
           ),
-          const SizedBox(height: 16,),
+          const SizedBox(height: 16),
           CustomAuthTextField(
             label: S.of(context).password,
             hint: '••••••••',
+            prefixIcon: Icons.lock_outlined,
+            widget:Text("Forget?", style: AppStyles.priceBold16(context)),
           ),
         ],
       ),
     );
   }
 }
-
