4ca4d8fe22b039f58f0a151fa11290bebfd4bc72
Abdulrahman <syamabdo382@gmail.com>
2026-04-07 19:42:03 +0200

    create openEmailApp method

--- lib/features/auth/presentation/views/widgets/open_email_button.dart
+++ lib/features/auth/presentation/views/widgets/open_email_button.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:stock_mate/core/utils/widgets/custom_button.dart';
 import 'package:stock_mate/generated/l10n.dart';
 
 class OpenEmailButton extends StatelessWidget {
   const OpenEmailButton({super.key});
 
+  Future<void> openEmailApp() async {
+    // Gmail app or default email app
+    final Uri emailUri = Uri(scheme: 'mailto');
+
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomButton(
-      onPressed: () {
-        // open email App
-      },
+      onPressed: openEmailApp,
       isLoading: false,
       buttonName: S.of(context).openEmailApp,
       color: Colors.white,
