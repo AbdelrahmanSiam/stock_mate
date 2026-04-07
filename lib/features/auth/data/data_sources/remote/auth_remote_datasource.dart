21877cc042d9c3fb99ddb15745de13af91a64d7e
Abdulrahman <syamabdo382@gmail.com>
2026-04-07 14:12:12 +0200

    AuthRemoteDataSource

---
+++ lib/features/auth/data/data_sources/remote/auth_remote_datasource.dart
@@ -0,0 +1,20 @@
+import 'package:stock_mate/features/auth/data/models/user_model.dart';
+
+abstract class AuthRemoteDataSource {
+  Future<UserModel> login({required String email, required String password});
+
+  Future<UserModel> register({
+    required String email,
+    required String password,
+    required String displayName,
+    required String shopName,
+  });
+
+  Future<void> sendEmailVerification();
+
+  Future<bool> checkEmailVerified();
+
+  Future<void> sendPasswordResetEmail({required String email});
+
+  Future<void> logout();
+}
