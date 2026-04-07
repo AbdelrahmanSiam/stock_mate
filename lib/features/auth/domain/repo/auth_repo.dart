4d0fea1c3da420c67c47deca3b5876d471120126
Abdulrahman <syamabdo382@gmail.com>
2026-04-07 13:18:01 +0200

    create AuthRepository

---
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:stock_mate/core/errors/failure.dart';
+import 'package:stock_mate/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, UserEntity>> login({
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, UserEntity>> register({
+    required String email,
+    required String password,
+    required String displayName,
+  });
+
+  Future<Either<Failure, void>> sendEmailVerification();
+
+  Future<Either<Failure, bool>> checkEmailVerified();
+
+  Future<Either<Failure, void>> sendPasswordResetEmail({required String email});
+
+  Future<Either<Failure, void>> logout();
+}
