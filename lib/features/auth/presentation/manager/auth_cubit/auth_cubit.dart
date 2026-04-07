d6b240de6f7f86d16c0b59f2d90048fb34983190
Abdulrahman <syamabdo382@gmail.com>
2026-04-07 15:26:31 +0200

    Auth Cubit login method

--- lib/features/auth/presentation/manager/auth_cubit/auth_cubit.dart
+++ lib/features/auth/presentation/manager/auth_cubit/auth_cubit.dart
@@ -1,9 +1,41 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:stock_mate/features/auth/domain/entities/user_entity.dart';
+import 'package:stock_mate/features/auth/domain/use_cases/check_email_verified_usecase/check_email_verified_usecase.dart';
+import 'package:stock_mate/features/auth/domain/use_cases/login_use_case/login_parameters.dart';
+import 'package:stock_mate/features/auth/domain/use_cases/login_use_case/login_use_case.dart';
+import 'package:stock_mate/features/auth/domain/use_cases/register_use_case/register_use_case.dart';
+import 'package:stock_mate/features/auth/domain/use_cases/send_email_verification_usecase/send_email_verification_usecase.dart';
+import 'package:stock_mate/features/auth/domain/use_cases/send_password_reset_usecase/send_password_reset_usecase.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitialState());
+  AuthCubit(
+    this.loginUseCase,
+    this.registerUseCase,
+    this.sendEmailVerificationUseCase,
+    this.checkEmailVerifiedUseCase,
+    this.sendPasswordResetUseCase,
+  ) : super(AuthInitialState());
+  final LoginUseCase loginUseCase;
+  final RegisterUseCase registerUseCase;
+  final SendEmailVerificationUseCase sendEmailVerificationUseCase;
+  final CheckEmailVerifiedUseCase checkEmailVerifiedUseCase;
+  final SendPasswordResetUseCase sendPasswordResetUseCase;
+
+  Future<void> login({required String email, required String password}) async {
+    emit(AuthLoadingState());
+    final result = await loginUseCase.call(
+      LoginParameters(email: email, password: password),
+    );
+    result.fold(
+      (error) {
+        emit(AuthErrorState(error.errMessage));
+      },
+      (user) {
+        emit(AuthSuccessState(user));
+      },
+    );
+  }
 }
