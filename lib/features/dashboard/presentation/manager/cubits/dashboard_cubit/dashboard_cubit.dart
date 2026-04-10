bce8287d92298252e4ae912be97d7a78135a1f7b
Abdulrahman <syamabdo382@gmail.com>
2026-04-10 14:47:04 +0200

    create Dashboard Cubit method getDashboardData

--- lib/features/dashboard/presentation/manager/cubits/dashboard_cubit/dashboard_cubit.dart
+++ lib/features/dashboard/presentation/manager/cubits/dashboard_cubit/dashboard_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:stock_mate/features/dashboard/domain/entites/dashboard_entity.dart';
+import 'package:stock_mate/features/dashboard/domain/use_case/get_dashboard_usecase.dart';
 
 part 'dashboard_state.dart';
 
 class DashboardCubit extends Cubit<DashboardState> {
-  DashboardCubit() : super(DashboardInitialState());
+  final GetDashboardUseCase getDashboardUseCase;
+  DashboardCubit({required this.getDashboardUseCase})
+    : super(DashboardInitialState());
+
+  Future<void> getDashboardData() async {
+    emit(DashboardLoadingState());
+    final result = await getDashboardUseCase.call();
+    result.fold(
+      (failure) => emit(DashboardErrorState(failure.errMessage)),
+      (dashboard) => emit(DashboardLoadedState(dashboard)),
+    );
+  }
 }
