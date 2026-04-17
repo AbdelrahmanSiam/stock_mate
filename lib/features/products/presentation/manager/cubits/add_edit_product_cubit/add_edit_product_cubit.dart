6e836c60fb524db4d035bd421200cf06b71e1682
Abdulrahman <syamabdo382@gmail.com>
2026-04-17 14:13:53 +0200

    add uploadImage method and update saveProduct method into cubit

--- lib/features/products/presentation/manager/cubits/add_edit_product_cubit/add_edit_product_cubit.dart
+++ lib/features/products/presentation/manager/cubits/add_edit_product_cubit/add_edit_product_cubit.dart
@@ -1,16 +1,42 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:stock_mate/features/products/domain/entities/product_entity.dart';
 import 'package:stock_mate/features/products/domain/use_cases/add_product_use_case/add_product_use_case.dart';
 import 'package:stock_mate/features/products/domain/use_cases/add_product_use_case/add_products_use_case_parameters.dart';
+import 'package:stock_mate/features/products/domain/use_cases/delete_product_image_use_case/delete_product_image_use_case_parameters.dart';
+import 'package:stock_mate/features/products/domain/use_cases/delete_product_image_use_case/delete_product_image_usecase.dart';
+import 'package:stock_mate/features/products/domain/use_cases/update_product_use_case/update_product_use_case_parameters.dart';
+import 'package:stock_mate/features/products/domain/use_cases/update_product_use_case/update_product_usecase.dart';
+import 'package:stock_mate/features/products/domain/use_cases/upload_product_image_use_case/upload_product_image_use_case.dart';
+import 'package:stock_mate/features/products/domain/use_cases/upload_product_image_use_case/upload_product_image_use_case_parameters.dart';
 import 'package:uuid/uuid.dart';
 
 part 'add_edit_product_state.dart';
 
 class AddEditProductCubit extends Cubit<AddEditProductState> {
   final AddProductUseCase addProductUseCase;
-  AddEditProductCubit(this.addProductUseCase)
-    : super(AddEditProductInitialState());
+  final UpdateProductUseCase updateProductUseCase;
+  final UploadProductImageUseCase uploadProductImageUseCase;
+  final DeleteProductImageUseCase deleteProductImageUseCase;
+  AddEditProductCubit(
+    this.addProductUseCase,
+    this.uploadProductImageUseCase,
+    this.updateProductUseCase,
+    this.deleteProductImageUseCase,
+  ) : super(AddEditProductInitialState());
+
+  Future<void> uploadImage(File image) async {
+    emit(AddEditProductImageUploadingState());
+    final result = await uploadProductImageUseCase.call(
+      UploadProductImageUseCaseParameters(image: image),
+    );
+    result.fold(
+      (failure) => emit(AddEditProductErrorState(failure.errMessage)),
+      (url) => emit(AddEditProductImageUploadedState(url)),
+    );
+  }
 
   Future<void> saveProduct({
     required String? existingId,
@@ -22,6 +48,7 @@ class AddEditProductCubit extends Cubit<AddEditProductState> {
     required int quantity,
     required int threshold,
     required String imageUrl,
+    String? oldImageUrl,
   }) async {
     emit(AddEditProductSavingState());
     final String productId = existingId ?? const Uuid().v4();
@@ -36,14 +63,28 @@ class AddEditProductCubit extends Cubit<AddEditProductState> {
       threshold: threshold,
       imageUrl: imageUrl,
     );
-    final result = await addProductUseCase.call(
-      AddProductsUseCaseParameters(product: product),
-    );
+    final bool isEdit =
+        existingId != null; // isEdit is true when there are existing Id already
+    final result = isEdit
+        ? await updateProductUseCase.call(
+            UpdateProductUseCaseParameters(product: product),
+          )
+        : await addProductUseCase.call(
+            AddProductsUseCaseParameters(product: product),
+          );
     result.fold(
       (error) {
         emit(AddEditProductErrorState(error.errMessage));
       },
       (_) {
+        if (isEdit &&
+            oldImageUrl != null &&
+            oldImageUrl.isNotEmpty &&
+            oldImageUrl != imageUrl) {
+          deleteProductImageUseCase.call(
+            DeleteProductImageUseCaseParameters(imageUrl: oldImageUrl),
+          );
+        }
         emit(AddEditProductSavedState());
       },
     );
