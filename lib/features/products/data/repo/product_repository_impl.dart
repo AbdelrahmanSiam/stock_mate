e661ab9ce38326270b64f0fcff27922daec734cf
Abdulrahman <syamabdo382@gmail.com>
2026-04-17 11:54:16 +0200

    Finish ProductRepositoryImpl methods

--- lib/features/products/data/repo/product_repository_impl.dart
+++ lib/features/products/data/repo/product_repository_impl.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:stock_mate/core/errors/failure.dart';
+import 'package:stock_mate/features/products/data/data_sources/remote/product_image_datasource/product_image_datasource.dart';
 import 'package:stock_mate/features/products/data/data_sources/remote/product_remote_datasource/product_remote_datasource.dart';
 import 'package:stock_mate/features/products/data/models/product_model.dart';
 import 'package:stock_mate/features/products/data/repo/helper/helper.dart';
@@ -8,12 +11,48 @@ import 'package:stock_mate/features/products/domain/repo/product_repository.dart
 
 class ProductRepositoryImpl implements ProductRepository {
   final ProductRemoteDatasource remoteDatasource;
+  final ProductImageDataSource productImageDataSource;
 
-  const ProductRepositoryImpl({required this.remoteDatasource});
+  const ProductRepositoryImpl({
+    required this.remoteDatasource,
+    required this.productImageDataSource,
+  });
   @override
   Future<Either<Failure, void>> addProduct(ProductEntity product) {
     return handleFirebaseFirestoreRequests(
       () => remoteDatasource.addProduct(product as ProductModel),
     );
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteProduct(String id) {
+    return handleFirebaseFirestoreRequests(
+      () => remoteDatasource.deleteProduct(id),
+    );
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteProductImage(String imageUrl) async {
+    try {
+      return Right(await productImageDataSource.deleteImage(imageUrl));
+    } catch (e) {
+      return Left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> updateProduct(ProductEntity product) {
+    return handleFirebaseFirestoreRequests(
+      () => remoteDatasource.updateProduct(product as ProductModel),
+    );
+  }
+
+  @override
+  Future<Either<Failure, String>> uploadProductImage(File image) async {
+    try {
+      return Right(await productImageDataSource.uploadImage(image));
+    } catch (e) {
+      return Left(ServerFailure(errMessage: e.toString()));
+    }
   }
 }
