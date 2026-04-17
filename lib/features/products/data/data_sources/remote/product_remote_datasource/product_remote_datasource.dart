4b9a9f18fc0ba8cf39fb3c4b0b4e6355a9f76208
Abdulrahman <syamabdo382@gmail.com>
2026-04-17 11:41:29 +0200

    add update and delete product methods to ProductRemoteDatasource abstract class

--- lib/features/products/data/data_sources/remote/product_remote_datasource/product_remote_datasource.dart
+++ lib/features/products/data/data_sources/remote/product_remote_datasource/product_remote_datasource.dart
@@ -2,4 +2,6 @@ import 'package:stock_mate/features/products/data/models/product_model.dart';
 
 abstract class ProductRemoteDatasource {
   Future<void> addProduct(ProductModel product);
+  Future<void> updateProduct(ProductModel product);
+  Future<void> deleteProduct(String id);
 }
