e7889de0b3db52b5eac2eec4db66369b3c4d0064
Abdulrahman <syamabdo382@gmail.com>
2026-04-20 11:56:46 +0200

    create InvoiceItemEntity

---
+++ lib/features/sales/domain/entites/invoice_item_entity.dart
@@ -0,0 +1,32 @@
+class InvoiceItemEntity {
+  final String productId;
+  final String productName;
+  final String productImageUrl;
+  final double unitPrice;
+  final int quantity;
+  final int
+  availableStock; // validation to know current amount when user increase
+
+  const InvoiceItemEntity({
+    required this.productId,
+    required this.productName,
+    required this.productImageUrl,
+    required this.unitPrice,
+    required this.quantity,
+    required this.availableStock,
+  });
+
+  double get totalPrice => unitPrice * quantity;
+
+  // copyWith for quantity updates
+  InvoiceItemEntity copyWith({int? quantity}) => InvoiceItemEntity(
+    productId: productId,
+    productName: productName,
+    productImageUrl: productImageUrl,
+    unitPrice: unitPrice,
+    quantity: quantity ?? this.quantity,
+    availableStock: availableStock,
+  );
+
+  bool get canIncrement => quantity < availableStock;
+}
