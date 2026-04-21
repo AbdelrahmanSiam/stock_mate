99207d422220cc1b3c1626168f3c83952762cda5
Abdulrahman <syamabdo382@gmail.com>
2026-04-21 10:42:10 +0200

    add init method to cubit

--- lib/features/sales/presentation/manager/cubits/sale_cubit/sale_cubit.dart
+++ lib/features/sales/presentation/manager/cubits/sale_cubit/sale_cubit.dart
@@ -3,9 +3,24 @@ import 'package:meta/meta.dart';
 import 'package:stock_mate/features/products/domain/entities/product_entity.dart';
 import 'package:stock_mate/features/sales/domain/entities/invoice_item_entity.dart';
 import 'package:stock_mate/features/sales/domain/entities/sale_entity.dart';
+import 'package:stock_mate/features/sales/domain/use_case/create_sale_use_case/create_sale_use_case.dart';
 
 part 'sale_state.dart';
 
 class SaleCubit extends Cubit<SaleState> {
-  SaleCubit() : super(SaleInitialState());
+  final CreateSaleUseCase createSaleUseCase;
+  List<ProductEntity> allProducts = [];
+  SaleCubit(this.createSaleUseCase) : super(SaleInitialState());
+
+  void init(List<ProductEntity> products) {
+    allProducts = products;
+    emit(
+      SaleItemsUploadedState(
+        invoiceItems: {},
+        searchResults: [],
+        searchQuery: "",
+        paymentMethod: "Cash",
+      ),
+    );
+  }
 }
