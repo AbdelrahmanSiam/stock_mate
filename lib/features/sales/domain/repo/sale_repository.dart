a1b1f7c399d583493155198a29f0e8693ef65f64
Abdulrahman <syamabdo382@gmail.com>
2026-04-28 09:37:47 +0300

    Add getRecentSales method to Sale Repo

--- lib/features/sales/domain/repo/sale_repository.dart
+++ lib/features/sales/domain/repo/sale_repository.dart
@@ -1,11 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:stock_mate/core/errors/failure.dart';
+import 'package:stock_mate/features/dashboard/domain/entities/recent_sale_entity.dart';
 import 'package:stock_mate/features/sales/domain/entities/invoice_item_entity.dart';
 import 'package:stock_mate/features/sales/domain/entities/sale_entity.dart';
+import 'package:stock_mate/features/sales/domain/entities/sale_filter.dart';
 
 abstract class SaleRepository {
   Future<Either<Failure, SaleEntity>> createSale({
     required List<InvoiceItemEntity> items,
     required String paymentMethod,
+  });
+  Stream<Either<Failure, RecentSaleEntity>> getRecentSales({
+    required SaleFilter filter,
   });
 }
