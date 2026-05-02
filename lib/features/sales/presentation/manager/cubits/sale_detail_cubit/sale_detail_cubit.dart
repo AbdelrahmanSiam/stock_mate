2bed6d4dbdffffa80c4516a70008f4bbb2eb26a8
Abdulrahman <syamabdo382@gmail.com>
2026-05-02 10:11:57 +0300

    Add getSale to Cubit

--- lib/features/sales/presentation/manager/cubits/sale_detail_cubit/sale_detail_cubit.dart
+++ lib/features/sales/presentation/manager/cubits/sale_detail_cubit/sale_detail_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:stock_mate/features/sales/domain/entities/sale_entity.dart';
+import 'package:stock_mate/features/sales/domain/use_case/get_sale_by_id_use_case/get_sale_by_id_use_case.dart';
+import 'package:stock_mate/features/sales/domain/use_case/get_sale_by_id_use_case/get_sale_by_id_use_case_parameters.dart';
 
 part 'sale_detail_state.dart';
 
 class SaleDetailCubit extends Cubit<SaleDetailState> {
-  SaleDetailCubit() : super(SaleDetailInitialState());
+  final GetSaleByIdUseCase getSaleByIdUseCase;
+  SaleDetailCubit(this.getSaleByIdUseCase) : super(SaleDetailInitialState());
+
+  Future<void> getSale(String saleId) async {
+    emit(SaleDetailLoadingState());
+    final result = await getSaleByIdUseCase.call(
+      GetSaleByIdUseCaseParameters(saleId: saleId),
+    );
+    result.fold(
+      (failure) => emit(SaleDetailErrorState(errMessage: failure.errMessage)),
+      (sale) => emit(SaleDetailSuccessState(sale: sale)),
+    );
+  }
 }
