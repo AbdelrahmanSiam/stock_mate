9d7722ad0efa15f879fb7756ac5244ed05be8be6
Abdulrahman <syamabdo382@gmail.com>
2026-05-11 05:55:41 +0300

    Add PDF states to Sale Detail Cubit

--- lib/features/sales/presentation/manager/cubits/sale_detail_cubit/sale_detail_state.dart
+++ lib/features/sales/presentation/manager/cubits/sale_detail_cubit/sale_detail_state.dart
@@ -18,3 +18,24 @@ final class SaleDetailErrorState extends SaleDetailState {
 
   SaleDetailErrorState({required this.errMessage});
 }
+
+//                                 PDF states
+
+final class SaleDetailPdfGeneratingState extends SaleDetailState {
+  final SaleEntity
+  sale; // Need sale because UI will not change and we can show a loading indicator on top of the current details
+  SaleDetailPdfGeneratingState(this.sale);
+}
+
+final class SaleDetailPdfReadyState extends SaleDetailState {
+  final SaleEntity sale;
+  final String filePath;
+  SaleDetailPdfReadyState({required this.sale, required this.filePath});
+}
+
+final class SaleDetailPdfErrorState extends SaleDetailState {
+  final SaleEntity
+  sale; // Need sale for retrying PDF generation without refetching sale details
+  final String errMessage;
+  SaleDetailPdfErrorState({required this.sale, required this.errMessage});
+}
