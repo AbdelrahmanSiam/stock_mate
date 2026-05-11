05d6556a10f7c7d3d795770c017870036d5fcb2c
Abdulrahman <syamabdo382@gmail.com>
2026-05-11 16:08:53 +0300

    create ShopInfoCard

--- lib/features/setting/presentation/views/widgets/setting_view_body.dart
+++ lib/features/setting/presentation/views/widgets/setting_view_body.dart
@@ -1,14 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:stock_mate/core/styles/app_styles.dart';
+import 'package:stock_mate/core/theme/app_colors/app_colors_dark_mode.dart';
+import 'package:stock_mate/core/utils/widgets/app_card.dart';
+import 'package:stock_mate/core/utils/widgets/custom_button.dart';
+import 'package:stock_mate/core/utils/widgets/custom_text_field.dart';
+import 'package:stock_mate/core/utils/widgets/custom_user_avatar.dart';
 import 'package:stock_mate/features/setting/presentation/views/widgets/setting_view_header.dart';
+import 'package:stock_mate/features/setting/presentation/views/widgets/shop_info_card.dart';
+import 'package:stock_mate/generated/l10n.dart';
 
-class SettingViewBody extends StatelessWidget {
+class SettingViewBody extends StatefulWidget {
   const SettingViewBody({super.key});
+
+  @override
+  State<SettingViewBody> createState() => _SettingViewBodyState();
+}
+
+class _SettingViewBodyState extends State<SettingViewBody> {
+  final TextEditingController controller = TextEditingController();
+  @override
+  void initState() {
+    controller.text = "Abdoz Shop";
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
-      child: Column(children: [SettingViewHeader()]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SettingViewHeader(),
+          const SizedBox(height: 20),
+          ShopInfoCard(controller: controller),
+        ],
+      ),
     );
   }
 }
---
+++ lib/features/setting/presentation/views/widgets/shop_info_card.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:stock_mate/core/styles/app_styles.dart';
+import 'package:stock_mate/core/theme/app_colors/app_colors_dark_mode.dart';
+import 'package:stock_mate/core/utils/widgets/app_card.dart';
+import 'package:stock_mate/core/utils/widgets/custom_button.dart';
+import 'package:stock_mate/core/utils/widgets/custom_text_field.dart';
+import 'package:stock_mate/core/utils/widgets/custom_user_avatar.dart';
+import 'package:stock_mate/generated/l10n.dart';
+
+class ShopInfoCard extends StatelessWidget {
+  const ShopInfoCard({super.key, required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          S.of(context).shopInfo,
+          style: AppStyles.labelSemiBold13(
+            context,
+          ).copyWith(color: AppColorsDarkMode.textSecondary),
+        ),
+        SizedBox(height: 10),
+        AppCard(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          child: Column(
+            children: [
+              SizedBox(child: CustomUserAvatar()),
+              const SizedBox(height: 30),
+              Text("Abdoz Shop", style: AppStyles.appNameBold32(context)),
+              const SizedBox(height: 3),
+              Text(
+                "ID : SHOP-8255",
+                style: AppStyles.buttonSemiBold15(
+                  context,
+                ).copyWith(color: AppColorsDarkMode.textSecondary),
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                label: S.of(context).shopName,
+                hint: "",
+                prefixIcon: Icons.store,
+                controller: controller,
+              ),
+              const SizedBox(height: 10),
+              CustomButton(
+                onPressed: () {},
+                isLoading: false,
+                buttonName: S.of(context).saveChanges,
+                iconButton: Icons.save,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
