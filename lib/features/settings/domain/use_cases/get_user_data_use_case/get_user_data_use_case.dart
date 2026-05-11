11fd3a8e3dc7c91768f84a2cd374e56b7e757c14
Abdulrahman <syamabdo382@gmail.com>
2026-05-11 19:23:43 +0300

    create GetUserDataUseCase

---
+++ lib/features/settings/domain/use_cases/get_user_data_use_case/get_user_data_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:stock_mate/core/errors/failure.dart';
+import 'package:stock_mate/core/usecases/use_case.dart';
+import 'package:stock_mate/features/settings/domain/enitites/settings_user_entity.dart';
+import 'package:stock_mate/features/settings/domain/repo/settings_repository.dart';
+
+class GetUserDataUseCase implements UseCase<SettingsUserEntity, NoParameters> {
+  final SettingsRepository repository;
+
+  GetUserDataUseCase({required this.repository});
+  @override
+  Future<Either<Failure, SettingsUserEntity>> call([NoParameters? param]) {
+    return repository.getUserData();
+  }
+}
